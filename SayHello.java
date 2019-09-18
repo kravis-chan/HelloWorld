554f18dbb4ca7d271a226433245094c6dad17b14
unknown <you@example.com>
2019-09-19 00:49:54 +0800

    created a sayhello class

---
+++ SayHello.java
@@ -0,0 +1,18 @@
+public class SayHello{
+	public static void main(String[] arge) {
+		System.out.println("Bellow is the function of sayhello");
+		SayHello();
+	}
+	
+	public static void SayHello(){
+		System.out.println("say hello!" + Sum());
+	}
+	
+	public static int Sum(){
+		int sum=0;
+		for (int i = 1;i <= 100;i++ ){
+			sum = sum + i;
+		} 
+		return sum;
+	}
+}
