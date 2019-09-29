843b33437e5e68603911d1d8aa63c9db267753f9
unknown <you@example.com>
2019-09-30 00:05:41 +0800

    ObjectArray class

---
+++ ObjectArray.java
@@ -0,0 +1,15 @@
+class Value{
+	int i;
+}
+
+public class ObjectArray {
+	public static void main (String[] args){
+		Value[] av = new Value[ (int)(Math.random() * 10) + 1];
+		for(int i = 0;i < av.length;++i){
+			av[i] = new Value90;
+			av[i] = (int)(Math.random() * 100);
+		}
+		for(Value v : av)
+			System.out.println(v.i);
+	}
+}
