0ec53667b2fc82421300292d6f2625cd127c8b9b
unknown <you@example.com>
2019-09-30 14:21:40 +0800

    Array Sort class

---
+++ ArraySort.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class ArraySort{
+	public static void main(String[] args){
+		int a[] = {8,7,9,67,38,3,329,20,5};
+		Arrays.sort(a);
+		for(int i = 0;i < a.length;i++)
+			System.out.println(a[i] + "");
+	}
+}
